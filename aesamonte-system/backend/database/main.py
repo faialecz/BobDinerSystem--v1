1359dded6cdc0db435fa9c788f6e568083cc7503
lnaniwowen <155816038+lnaniwowen@users.noreply.github.com>
2026-01-23 06:28:48 +0800

    Supabase instead of PostgreSQL only

---
+++ aesamonte-system/backend/database/main.py
@@ -0,0 +1,47 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+for var in ("PGUSER", "PGPASSWORD", "PGHOST", "PGPORT", "PGDATABASE"):
+    os.environ.pop(var, None)
+    
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+
+print(USER, HOST, PORT, DBNAME)
+
+# Connect to the database
+try:
+    connection = psycopg2.connect(
+        user=USER,
+        password=PASSWORD,
+        host=HOST,
+        port=PORT,
+        dbname=DBNAME,
+        sslmode="require"
+    )
+    print("Connection successful!")
+    
+    # Create a cursor to execute SQL queries
+    cursor = connection.cursor()
+    
+    # Example query
+    cursor.execute("SELECT NOW();")
+    result = cursor.fetchone()
+    print("Current Time:", result)
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+    print("Connection closed.")
+
+except Exception as e:
+    print(f"Failed to connect: {e}")
